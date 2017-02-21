92c8f2e9f094024973b1642c43de5189a7a4868c
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-02-21 22:46:14 +0100

    Updated collisions to the new simplified API

--- boxBoxCollision2D.js
+++ boxBoxCollision2D.js
@@ -1,16 +1,11 @@
 CLOCKWORKRT.collisions.push({
-    shapes: ["box"],
-    detectors: [
-        {
-            shape1: "box",
-            shape2: "box",
-            detector: function (b1, b2, data) {
-                if (!((b1.x + b1.w) > b2.x && (b2.x + b2.w) > b1.x && (b1.z + b1.h) > b2.z && (b2.z + b2.h) > b1.z)) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
+    shape1: "box",
+    shape2: "box",
+    detector: function (b1, b2, data) {
+        if (!((b1.x + b1.w) > b2.x && (b2.x + b2.w) > b1.x && (b1.z + b1.h) > b2.z && (b2.z + b2.h) > b1.z)) {
+            return false;
+        } else {
+            return true;
         }
-    ]
+    }
 });
