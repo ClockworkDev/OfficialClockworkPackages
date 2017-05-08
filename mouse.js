b4545036908b265b094c4480bffc2677ebe1b69b
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-05-08 18:15:58 +0200

    Mouse component

--- mouse.js
+++ mouse.js
@@ -1,80 +1,76 @@
 CLOCKWORKRT.components.register([
-{
-    name: "mouse",
-    events: [
-        {
-            name: "#setup", code: function (event) {
-                this.setVar("listener_click", this.engine.getEngineVar("#DOM").addEventListener("mousedown",this.do.onclick.bind(this), false));
-                this.setVar("listener_move", this.engine.getEngineVar("#DOM").addEventListener("mousemove", this.do.onmove.bind(this), false));
-                this.setVar("listener_up", this.engine.getEngineVar("#DOM").addEventListener("mouseup", this.do.mouseup.bind(this), false));
-            }
-        },
-        {
-            name: "#exit", code: function (event) {
-                this.engine.getEngineVar("#DOM").removeEventListener("click", this.getVar("listener_click"));
-                this.engine.getEngineVar("#DOM").removeEventListener("mousemove", this.getVar("listener_move"));a
-                this.engine.getEngineVar("#DOM").removeEventListener("mouseup", this.getVar("listener_up"));
-            }
-        },
-        {
-            name: "onclick", code: function (e) {
-                this.setVar("timer", -1);
-                //Warning: This will only work if there is no scaling between the game coordinates and the canvas!
-                //If you are using a custom Spritesheet.js renderMode, you will need to do something like this:
-                //
-                //cx=  e.clientX / window.innerWidth * 1366;
-                //cy = (e.clientY - (window.innerHeight - 768 * window.innerWidth / 1366) / 2) * 1366 / window.innerWidth
-                //
-                // Where 1366x768 is the size of the buffer and the virtual game screens
-            }
-        },
-         {
-             name: "onmove", code: function (e) {
-                 var tx = e.offsetX == undefined ? e.layerX : e.offsetX;
-                 var ty = e.offsetY == undefined ? e.layerY : e.offsetY;
+    {
+        name: "mouse",
+        description: "This component allows you to incorporate mouse support to your game, via two point collisions for the mouse hover and click.",
+        events: [
+            {
+                name: "#setup", code: function (event) {
+                    var manifest = CLOCKWORKRT.API.getManifest();
+                    this.var.w = manifest.screenResolution.w;
+                    this.var.h = manifest.screenResolution.h;
+                    this.var.histener_click = this.engine.var["#DOM"].addEventListener("mousedown", this.do.onclick.bind(this), false);
+                    this.var.listener_move = this.engine.var["#DOM"].addEventListener("mousemove", this.do.onmove.bind(this), false);
+                    this.var.listener_up = this.engine.var["#DOM"].addEventListener("mouseup", this.do.mouseup.bind(this), false);
+                }
+            },
+            {
+                name: "#exit", code: function (event) {
+                    this.engine.var["#DOM"].removeEventListener("click", this.var.listener_click);
+                    this.engine.var["#DOM"].removeEventListener("mousemove", this.var.listener_move);
+                    this.engine.var["#DOM"].removeEventListener("mouseup", this.var.listener_up);
+                }
+            },
+            {
+                name: "onclick", code: function (e) {
+                    this.var.timer = -1;
+                }
+            },
+            {
+                name: "onmove", code: function (e) {
+                    var tx = e.offsetX == undefined ? e.layerX : e.offsetX;
+                    var ty = e.offsetY == undefined ? e.layerY : e.offsetY;
 
-                //  if (window.innerWidth / CLOCKWORKCONFIG.screenbuffer_width < window.innerHeight / CLOCKWORKCONFIG.screenbuffer_width) {
-                //      tx = CLOCKWORKCONFIG.screenbuffer_width * tx / window.innerWidth;
-                //      var ypos = (window.innerHeight - CLOCKWORKCONFIG.screenbuffer_height * window.innerWidth / CLOCKWORKCONFIG.screenbuffer_width) / 2;
-                //      var height = (CLOCKWORKCONFIG.screenbuffer_height * window.innerWidth / CLOCKWORKCONFIG.screenbuffer_width);
-                //      ty = CLOCKWORKCONFIG.screenbuffer_height * (ty - ypos) / height;
-                //  } else {
-                //      ty = CLOCKWORKCONFIG.screenbuffer_height * ty / window.innerHeight;
-                //      var xpos = (window.innerWidth - CLOCKWORKCONFIG.screenbuffer_width * window.innerHeight / CLOCKWORKCONFIG.screenbuffer_height) / 2;
-                //      var width = (CLOCKWORKCONFIG.screenbuffer_width * window.innerHeight / CLOCKWORKCONFIG.screenbuffer_height);
-                //      tx = CLOCKWORKCONFIG.screenbuffer_width * (tx - xpos) / width;
-                //  }
-                 var camera=this.engine.getAnimationEngine().getCamera();
-                 this.setVar("#x",+tx + +camera.x);
-                 this.setVar("#y",+ty + +camera.y);
-                 //Warning: Read the previous warning
-             }
-         },
-         {
-             name: "#loop", code: function (event) {
-                 //We wait one iteration before deleting the click coordinates
-                 if (this.getVar("timer") == 1) {
-                     this.collision["point"][1].x = NaN;
-                     this.collision["point"][1].y = NaN;
-                     this.collisionChanged();
-                     this.setVar("timer", 0);
-                 }
-                 if (this.getVar("timer") ==-1) {
-                     this.collision["point"][1].x=this.getVar("#x");
-                     this.collision["point"][1].y = this.getVar("#y");
-                     this.collisionChanged();
-                     this.setVar("timer", 1);
-                 }
-             }
-         }
-    ],
-    collision: {
-        "point": [
-            //Coordinates of the pointer
-            { "x": 0, "y": 0 },
-            //Coordinates of the click
-            { "x": NaN, "y": NaN }
-        ]
+                    //Transform the coordinates to the Spritesheet.js canvas
+                    tx = this.var.w * tx / window.innerWidth;
+                    var ypos = (window.innerHeight - this.var.h * window.innerWidth / this.var.w) / 2;
+                    var height = (this.var.h * window.innerWidth / this.var.w);
+                    ty = this.var.h * (ty - ypos) / height;
+
+                    var camera = this.engine.getAnimationEngine().getCamera();
+                    this.var.$x = +tx + camera.x;
+                    this.var.$y = +ty + camera.y;
+
+                }
+            },
+            {
+                name: "#loop", code: function (event) {
+                    //We wait one iteration before deleting the click coordinates
+                    if (this.var.timer == 1) {
+                        //Remove clic collision
+                        this.setCollider("click", {
+                            x: NaN,
+                            y: NaN
+                        });
+                        this.var.timer = 0;
+                    }
+                    if (this.var.timer == -1) {
+                        //Set click collision
+                        this.setCollider("click", {
+                            x: 0,
+                            y: 0
+                        });
+                        this.var.timer = 1;
+                    }
+                }
+            }
+        ],
+        collision: {
+            "point": [
+                //Coordinates of the pointer
+                { "#tag": "hover", "x": 0, "y": 0 },
+                //Coordinates of the click
+                { "#tag": "click", "x": NaN, "y": NaN }
+            ]
+        }
     }
-}
 ]);
